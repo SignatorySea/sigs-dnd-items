dadf5f7f9735258006c964a53440db7405d64aaa
signatorysea <152825796+SignatorySea@users.noreply.github.com>
2026-05-19 13:35:13 -0400

    Valda's Spire of Secrets Necromancer Addition (still needs modifying)

---
+++ scripts/activity_icon_defaults.js
@@ -0,0 +1,23 @@
+Hooks.once("init", () => {
+  const activityIcons = {
+    attack:   "icons/skills/melee/strike-sword-steel-yellow.webp",
+    cast:     "icons/magic/fire/flame-burning-hand-orange.webp",
+    check:    "icons/skills/social/peace-luck-insult.webp",
+    damage:   "icons/magic/fire/flame-burning-embers-orange.webp",
+    enchant:  "icons/magic/symbols/runes-etched-steel-blade.webp",
+    forward:  "icons/magic/movement/trail-streak-zigzag-teal.webp",
+    heal:     "icons/magic/life/cross-explosion-burst-green.webp",
+    save:     "icons/magic/light/beam-impact-deflect-teal.webp",
+    summon:   "icons/magic/movement/chevrons-down-yellow.webp",
+    transform:"icons/creatures/mammals/bull-horns-eyes-glowin-orange.webp",
+    utility:  "icons/skills/movement/ball-spinning-blue.webp",
+  };
+
+  // Apply icons to activityTypes
+   for (const [key, img] of Object.entries(activityIcons)) {
+    const activity = CONFIG.DND5E.activityTypes[key];
+    activity.documentClass.metadata.img = img
+  }
+
+  console.log("images loaded?");
+});
---
+++ scripts/modifying_5e.js
@@ -0,0 +1,60 @@
+// Add a new Spell School that can be selected in Spell Items
+/* Hooks.once("init", () => {
+  CONFIG.DND5E.spellSchools.psionics = {
+    label: "Psionics",
+    icon: "…",
+    fullKey: "psionics", // Full key used in enrichers
+    reference: "" // UUID of journal entry page for rich tooltips
+  };
+}); */
+
+
+Hooks.once("init", () => {
+
+  //new spell schools
+  CONFIG.DND5E.spellSchools.angelic = {
+        label: "Angelic",
+        icon: "…",
+        fullKey: "angelic",
+        reference: "" // UUID of journal entry page for rich tooltips
+    };
+  CONFIG.DND5E.spellSchools.chaos = {
+      label: "Chaos",
+      icon: "…",
+      fullKey: "chaos",
+      reference: "" // UUID of journal entry page for rich tooltips
+    };
+    CONFIG.DND5E.spellSchools.chronomancy = {
+      label: "Chronomancy",
+      icon: "…",
+      fullKey: "chronomancy",
+      reference: "" // UUID of journal entry page for rich tooltips
+    };
+
+    //Valda's Spire of Secrets
+    CONFIG.DND5E.spellSchools.renaissance = {
+        label: "Renaissance",
+        icon: "…",
+        fullKey: "renaissance",
+        reference: "" // UUID of journal entry page for rich tooltips
+    };
+
+
+    //Grafting Feature type from Valda's Spire of Secrets
+    CONFIG.DND5E.featureTypes.grafts = {
+      label: "Monstrous Grafts",
+      subtypes:{
+        arm: "Arms",
+        external: "External",
+        head: "Head",
+        internal: "Internal",
+        legs: "Legs"
+      }
+      };
+
+      //arcane stitches class feature from Valda's Spire of Secrets
+      //reanimator necromancer feature
+      CONFIG.DND5E.featureTypes.class.subtypes.arcaneStitch = "Arcane Stitch";
+});
+
+
