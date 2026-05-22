714421e6dd51b17bcd673ecaa636a09b018cf482
signatorysea <152825796+SignatorySea@users.noreply.github.com>
2026-05-22 15:41:07 -0400

    weapon properties of 5e items rule refs.

--- scripts/modifying_5e.js
+++ scripts/modifying_5e.js
@@ -10,6 +10,20 @@
 
 
 Hooks.once("init", () => {
+    //---------------------------------------------------
+    // Dnd 5e Rules References!
+    // --------------------------------------------------
+    CONFIG.DND5E.rules.weaponammunition = "Compendium.sigs-dnd-items.rules.JournalEntry.3PDDa29bXVNGWX4p.JournalEntryPage.h2ce0NA3kxMdmshq";
+    CONFIG.DND5E.rules.weaponfinesse = "Compendium.sigs-dnd-items.rules.JournalEntry.3PDDa29bXVNGWX4p.JournalEntryPage.OIKLEqOxp34orYHV";
+    CONFIG.DND5E.rules.weaponheavy = "Compendium.sigs-dnd-items.rules.JournalEntry.3PDDa29bXVNGWX4p.JournalEntryPage.rRLLKlrlbjCZ0agA";
+    CONFIG.DND5E.rules.weaponlight = "Compendium.sigs-dnd-items.rules.JournalEntry.3PDDa29bXVNGWX4p.JournalEntryPage.fuDsQOVs9rZ26skE";
+    CONFIG.DND5E.rules.weaponloading = "Compendium.sigs-dnd-items.rules.JournalEntry.3PDDa29bXVNGWX4p.JournalEntryPage.rxPmF9Vi7CwJ5q0l";
+    CONFIG.DND5E.rules.weaponrange = "Compendium.sigs-dnd-items.rules.JournalEntry.3PDDa29bXVNGWX4p.JournalEntryPage.AAwc6FEe3eOiHqgO";
+    CONFIG.DND5E.rules.weaponreach = "Compendium.sigs-dnd-items.rules.JournalEntry.3PDDa29bXVNGWX4p.JournalEntryPage.cbVIfqiuUbhQvzzC";
+    CONFIG.DND5E.rules.weaponthrown = "Compendium.sigs-dnd-items.rules.JournalEntry.3PDDa29bXVNGWX4p.JournalEntryPage.RcrxbGWBX92YAVMK";
+    CONFIG.DND5E.rules.weapontwohanded = "Compendium.sigs-dnd-items.rules.JournalEntry.3PDDa29bXVNGWX4p.JournalEntryPage.nJm4m3BE8cR8CD8N";
+    CONFIG.DND5E.rules.weaponversatile = "Compendium.sigs-dnd-items.rules.JournalEntry.3PDDa29bXVNGWX4p.JournalEntryPage.yxAPzqEA3jHfOlfq";
+
 
     //Grafting Feature type from Valda's Spire of Secrets
     CONFIG.DND5E.featureTypes.grafts = {
